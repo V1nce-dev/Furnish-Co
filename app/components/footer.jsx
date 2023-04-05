11b749a07b7b855f50b834c01303ad6357b26d68
Vincent <vsuen123@gmail.com>
2023-04-05 19:30:46 -0400

    added a footer

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,7 +1,57 @@
+"use client"
+import { AiOutlineMail } from "react-icons/ai";
+
 export default function Footer() {
-    return (
-        <div className="w-screen h-screen bg-black">
-            <h1>thing</h1>
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  const sitemap = ["ABOUT", "FAQs", "CONTACT", "COMPANY"];
+
+  return (
+    <footer className="bg-black text-white">
+      <div className="container m-auto p-5 py-10">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-5">
+          <div className="col-span-1">
+            <h3 className="text-3xl font-semibold mb-5">Furnish & Co</h3>
+            <p>Support the company and reach out.</p>
+            <div className="flex">
+              <input
+                type="text"
+                placeholder="Your@email.com"
+                className="mt-5 pl-4 outline-none rounded-lg text-black"
+              />
+              <button className="mt-5 ml-5 text-2xl">
+                <AiOutlineMail />
+              </button>
+            </div>
+          </div>
+          <div className="flex m-auto gap-40 text-lg font-mono font-bold">
+            {sitemap.map((item, index) => (
+              <h1
+                key={index}
+                onClick={handleScrollToTop}
+                className="hover:underline cursor-pointer"
+              >
+                {item}
+              </h1>
+            ))}
+          </div>
         </div>
-    )
+        <hr className="my-8" />
+        <div className="text-center text-sm">
+          <p>
+            Designed and developed by{" "}
+            <a
+              href="https://github.com/Lilswan77"
+              className="text-blue-500 font-bold hover:text-white"
+            >
+              Vince
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -1,22 +1,30 @@
+import { FaShoppingCart } from "react-icons/fa";
+
 export default function Header() {
   return (
-          <div>
+    <div>
       <nav className="fixed top-0 left-0 right-0 px-[10rem] py-5 z-10 bg-white drop-shadow-lg">
         <div className="flex justify-between items-center">
           <div className="text-black font-bold flex items-center">
-            My Website<span className="ml-10 text-sm text-gray-500">Shop</span>
+            Furnish & Co
+            <span className="ml-10 text-sm text-gray-500">Shop</span>
           </div>
-          <h1 className="text-black">Sign in</h1>
+          <div className="text-black font-bold flex items-center">
+            <span className="mr-10">
+              {" "}
+              <FaShoppingCart />{" "}
+            </span>
+            <h1 className="text-black">Sign in</h1>
+          </div>
         </div>
       </nav>
       <div className="mt-[5rem]">
         <nav className="px-[10rem]">
-          <div className="bg-gray-300 py-3 flex justify-center rounded-lg">
+          <div className="bg-[#808080] py-3 flex justify-center rounded-lg">
             <h1 className="text-white text-lg">Transform your home.</h1>
           </div>
         </nav>
       </div>
     </div>
-
   );
 }
